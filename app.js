2323e59a3864ec9539cf1179017380cb6a2e37be
RadhikaRJ <radhika7002@gmail.com>
2021-01-08 20:21:30 +0530

    feat: stock basic calculation logic

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-
-
+var unitPrice = document.getElementById("CP-unit");
+var qty = document.getElementById("qty");
+var todayprice = document.getElementById("current-CP");
+var outputref=document.querySelector("#output");
 var btnref = document.querySelector("#submit");
+
 btnref.addEventListener("click",clickEventHandler);
+
+function clickEventHandler(){
+    var unitp = unitPrice.value;
+    var qty_purchased = qty.value;
+    var latestPrice = todayprice.value;
+    var investedAmount = unitp*qty_purchased;
+    var todayreturns = qty_purchased*latestPrice;
+    if(todayreturns>investedAmount){
+        outputref.innerText="Congratulations!!\nYou have made a profit of "+ (((todayreturns-investedAmount)/investedAmount)*100)+"%";
+    }
+    else{
+        outputref.innerText="Oops! You've incurred a loss of "+ (((investedAmount-todayreturns)/investedAmount)*100)+ "%";
+    }
+}
 
