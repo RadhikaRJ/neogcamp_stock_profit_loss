a4ec8283f2c8481fa3164980d09d7331a83500b8
RadhikaRJ <radhika7002@gmail.com>
2021-01-08 20:53:11 +0530

    defect: empty input

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 var unitPrice = document.getElementById("CP-unit");
 var qty = document.getElementById("qty");
 var todayprice = document.getElementById("current-CP");
-var outputref=document.querySelector("#output");
+var outputref = document.querySelector("#output");
 var btnref = document.querySelector("#submit");
 
-btnref.addEventListener("click",clickEventHandler);
+btnref.addEventListener("click", clickEventHandler);
 
-function clickEventHandler(){
+function clickEventHandler() {
     var unitp = unitPrice.value;
     var qty_purchased = qty.value;
     var latestPrice = todayprice.value;
-    var investedAmount = unitp*qty_purchased;
-    var todayreturns = qty_purchased*latestPrice;
-    if(todayreturns>investedAmount){
-        outputref.style.color="green";
-        outputref.innerText="Congratulations!!\nYou have made a profit of "+ (((todayreturns-investedAmount)/investedAmount)*100)+"%";
+    if (unitp == '' || qty_purchased == '' || latestPrice == '') {
+        alert("Invalid/Missing input!");
+    } else {
+        var investedAmount = unitp * qty_purchased;
+        var todayreturns = qty_purchased * latestPrice;
+        if (todayreturns > investedAmount) {
+            outputref.style.color = "green";
+            outputref.innerText = "Congratulations!!\nYou have made a profit of " + (((todayreturns - investedAmount) / investedAmount) * 100) + "%";
+        } 
+        else {
+            outputref.style.color = "red";
+            outputref.innerText = "Oops! You've incurred a loss of " + (((investedAmount - todayreturns) / investedAmount) * 100) + "%";
+        }
     }
-    else{
-        outputref.style.color="red";
-        outputref.innerText="Oops! You've incurred a loss of "+ (((investedAmount-todayreturns)/investedAmount)*100)+ "%";
-    }
-}
 
+}
